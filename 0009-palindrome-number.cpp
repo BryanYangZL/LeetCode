1498afd17c279028ba2471763ae393a9dff6ca77
BryanYangZL <bryan.yangzl@foxmail.com>
2018-04-29 12:44:35 +0800

    9. Palindrome Number

---
+++ 0009-palindrome-number.cpp
@@ -0,0 +1,47 @@
+/*
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+
+Input: 121
+Output: true
+Example 2:
+
+Input: -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+*/
+
+#include<gtest/gtest.h>
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int palindrome = 0;
+        int cmp = x;
+
+        if (x < 0) return false;
+
+        while (x > 0) {
+            palindrome = palindrome * 10 + x % 10;
+            x /= 10;
+        }
+
+        return palindrome == cmp;
+    }
+};
+
+TEST(_0009, PalindromeNumer)
+{
+    Solution s;
+
+    ASSERT_EQ(s.isPalindrome(121), true);
+    ASSERT_EQ(s.isPalindrome(-121), false);
+    ASSERT_EQ(s.isPalindrome(10), false);
+}
+
